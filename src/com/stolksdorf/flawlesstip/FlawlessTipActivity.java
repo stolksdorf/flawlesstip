5204f8061fdf588fd3317022268322114a82b4cd
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2012-10-06 23:39:49 -0400

    First commit playa

---
+++ src/com/stolksdorf/flawlesstip/FlawlessTipActivity.java
@@ -0,0 +1,13 @@
+package com.stolksdorf.flawlesstip;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FlawlessTipActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
