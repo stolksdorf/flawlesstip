30ca1fb6cd0c3642d8568c5b94704b31940ec584
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2012-10-07 03:20:46 -0400

    Added some basic UI elements. Forgot how annoying android layouts can be

--- gen/com/stolksdorf/flawlesstip/R.java
+++ gen/com/stolksdorf/flawlesstip/R.java
@@ -8,16 +8,27 @@
 package com.stolksdorf.flawlesstip;
 
 public final class R {
+    public static final class array {
+        public static final int round_array=0x7f050001;
+        public static final int tip_array=0x7f050000;
+    }
     public static final class attr {
     }
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
+    public static final class id {
+        public static final int round_amount=0x7f060002;
+        public static final int textView1=0x7f060000;
+        public static final int tip_percentage=0x7f060001;
+    }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
+        public static final int amount_label=0x7f040003;
         public static final int app_name=0x7f040001;
+        public static final int appname=0x7f040002;
         public static final int hello=0x7f040000;
     }
 }
--- src/com/stolksdorf/flawlesstip/FlawlessTipActivity.java
+++ src/com/stolksdorf/flawlesstip/FlawlessTipActivity.java
@@ -2,6 +2,8 @@ package com.stolksdorf.flawlesstip;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class FlawlessTipActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +11,22 @@ public class FlawlessTipActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        
+        
+        Spinner tip_spinner = (Spinner) findViewById(R.id.tip_percentage);
+	    ArrayAdapter<CharSequence> tipAdapter = ArrayAdapter.createFromResource(this,
+	             R.array.tip_array, android.R.layout.simple_spinner_item);
+	    tipAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+	    tip_spinner.setAdapter(tipAdapter);	     
+	    tip_spinner.setSelection(1);
+        
+	    Spinner amount_spinner = (Spinner) findViewById(R.id.round_amount);
+	    ArrayAdapter<CharSequence> roundAdapter = ArrayAdapter.createFromResource(this,
+	             R.array.round_array, android.R.layout.simple_spinner_item);
+	    roundAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+	    amount_spinner.setAdapter(roundAdapter);	     
+	    amount_spinner.setSelection(1);
+	    
     }
 }
